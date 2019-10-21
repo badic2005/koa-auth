ddd9050b52e440d729501deb1b755b4072aed66b
badic <vadim021703@gmail.com>
2019-10-21 22:24:03 +0300

    templates tests refactoring

--- handlers/08-passport.js
+++ handlers/08-passport.js
@@ -1,7 +1,6 @@
 const passport = require('koa-passport')
 
 exports.init = app => {
-  // ctx.login, ctx.logout, ctx.isAuthenticated
   app.use(passport.initialize());
-  app.use(passport.session()); // ctx.state.user = user;
+  app.use(passport.session()); // ctx.state.user = user
 };
--- routes/logout.js
+++ routes/logout.js
@@ -1,5 +1,4 @@
 exports.post = async function(ctx, next) {
   ctx.logout();
-
   ctx.redirect('/');
 };
